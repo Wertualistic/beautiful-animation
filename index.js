9b4e321177e5cc9de6cae1a9f4f7122e472182fc
Wertualistic <abduxalilovmuhammadali1@gmail.com>
2023-01-17 09:49:49 +0500

    main

---
+++ index.js
@@ -0,0 +1,90 @@
+let img = document.querySelector('.img')
+
+window.addEventListener('scroll', ()=>{
+    const scrolled = window.scrollY;
+
+    if(scrolled){
+        img.style.position = 'fixed';
+        img.style.overflow = "hidden"
+        img.style.top = `20%`;
+        img.style.right = '0';
+    }
+} )
+
+let img1 = document.querySelector('.img1')
+
+window.addEventListener('scroll', ()=>{
+    const scrolled = window.scrollY;
+
+    if(scrolled){
+        img1.style.position = 'fixed';
+        img1.style.overflow = "hidden"
+        img1.style.top = `20%`;
+        img1.style.right = '0';
+    }
+} )
+
+let img2 = document.querySelector('.img2')
+
+window.addEventListener('scroll', ()=>{
+    const scrolled = window.scrollY;
+
+    if(scrolled){
+        img2.style.position = 'fixed';
+        img2.style.overflow = "hidden"
+        img2.style.top = `20%`;
+        img2.style.right = '0';
+    }
+} )
+
+let img3 = document.querySelector('.img3')
+
+window.addEventListener('scroll', ()=>{
+    const scrolled = window.scrollY;
+
+    if(scrolled){
+        img3.style.position = 'fixed';
+        img3.style.overflow = "hidden"
+        img3.style.top = `20%`;
+        img3.style.right = '0';
+    }
+} )
+
+let img4 = document.querySelector('.img4')
+
+window.addEventListener('scroll', ()=>{
+    const scrolled = window.scrollY;
+
+    if(scrolled){
+        img4.style.position = 'fixed';
+        img4.style.overflow = "hidden"
+        img4.style.top = `20%`;
+        img4.style.right = '0';
+    }
+} )
+
+let img5 = document.querySelector('.img5')
+
+window.addEventListener('scroll', ()=>{
+    const scrolled = window.scrollY;
+
+    if(scrolled){
+        img5.style.position = 'fixed';
+        img5.style.overflow = "hidden"
+        img5.style.top = `20%`;
+        img5.style.right = '0';
+    }
+} )
+
+let img6 = document.querySelector('.img6')
+
+window.addEventListener('scroll', ()=>{
+    const scrolled = window.scrollY;
+
+    if(scrolled){
+        img6.style.position = 'fixed';
+        img6.style.overflow = "hidden"
+        img6.style.top = `20%`;
+        img6.style.right = '0';
+    }
+} )
